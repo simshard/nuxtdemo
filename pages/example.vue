119627b03ea818d626556b392e838894ba78cd55
Simon K <30438951+simshard@users.noreply.github.com>
2023-10-05 16:14:24 +0100

    simple server api route

--- pages/example.vue
+++ pages/example.vue
@@ -13,7 +13,7 @@
 
     
   <div>
-<span>Users 1 local</span>
+<span>Users 1 local - client side</span>
     <ul>
       <li v-for="user in users" :key="user.id">
         {{ user.name }}
@@ -21,7 +21,7 @@
     </ul>
   </div>
   <div>
-<span>Users 2 SS</span>
+<span>Users 2 ServerSide Async</span>
     <ul>
       <li v-for="user in users2" :key="user.id">
         {{ user.name }}
@@ -30,7 +30,7 @@
   </div>
 
   <div>
-<span>Users 3 ss</span>
+<span>Users 3 ServerSide Await useFetch</span>
     <ul>
       <li v-for="user in users3" :key="user.id">
         {{ user.name }}
@@ -54,6 +54,7 @@
   })
   const { data: users2 } = await useAsyncData('users2', () => $fetch('https://jsonplaceholder.typicode.com/users'))
   const { data : users3 } = await useFetch('https://jsonplaceholder.typicode.com/users')
-  const { data : user } = await useFetch('https://jsonplaceholder.typicode.com/users/1' ,{ pick: ['id', 'name', 'email'] })
+  
+  const { data : user } = await useFetch('https://jsonplaceholder.typicode.com/users/1' ,{ pick: ['id', 'name'] })
 
 </script>
