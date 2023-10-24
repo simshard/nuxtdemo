c5687a06ac14be0853c8c62dec999272f8adea2b
Simon K <30438951+simshard@users.noreply.github.com>
2023-10-24 23:39:07 +0100

    base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    runtimeConfig: {
-    BASE_URL: process.env.BASE_URL,
+  runtimeConfig: {
+    BASE_URL: ,
   },
+   
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   build: {
