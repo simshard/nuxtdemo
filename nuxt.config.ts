5cf69b08fa00b3f75838b842a887662a23f4518a
Simon K <30438951+simshard@users.noreply.github.com>
2023-10-24 23:40:39 +0100

    fix ENV

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   runtimeConfig: {
-    BASE_URL: ,
+    BASE_URL: process.env.BASE_URL,
   },
    
   devtools: { enabled: true },
